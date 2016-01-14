f726399b63616e1280a6aac58ccf4dd5d8d8dffd
Kevin Cox <kevincox@kevincox.ca>
2016-01-14 07:45:41 -0500

    Add blog features.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :blogposts
 	# The priority is based upon order of creation: first created -> highest priority.
 	# See how all your routes lay out with "rake routes".
 	
@@ -54,5 +56,6 @@ Rails.application.routes.draw do
 	#     resources :products
 	#   end
 	
-	root 'application#hello'
+	root 'blogposts#index'
+	get 'hello' => 'application#hello'
 end
