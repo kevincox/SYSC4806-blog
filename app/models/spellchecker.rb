1197dfb5e1a7f52e09deaa3d2b7eaff1222cc636
Kevin Cox <kevincox@kevincox.ca>
2016-01-21 09:51:09 -0500

    Do lab 2.

--- app/models/spellchecker.rb
+++ app/models/spellchecker.rb
@@ -1,76 +1,126 @@
 require 'set'
 
 class Spellchecker
+	ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+	
+	attr_reader :dictionary
 
-  
-  ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+	def initialize(text_file_name)
+		@dictionary = Hash.new 0
+		
+		train_file! text_file_name
+	end
+	
+	# Returns an array of words in the text.
+	def words text
+		text.downcase.scan(/[a-z]+/)
+	end
 
-  #constructor.
-  #text_file_name is the path to a local file with text to train the model (find actual words and their #frequency)
-  #verbose is a flag to show traces of what's going on (useful for large files)
-  def initialize(text_file_name)
-    #read file text_file_name
-    #extract words from string (file contents) using method 'words' below.
-    #put in dictionary with their frequency (calling train! method)
-  end
+	# train model (create dictionary)
+	def train! word_list
+		word_list.each{|w| dictionary[w] += 1 }
+	end
+	
+	def train_file! path
+		train! words File.read path
+	end
 
-  def dictionary
-    #getter for instance attribute
-  end
-  
-  #returns an array of words in the text.
-  def words (text)
-    return text.downcase.scan(/[a-z]+/) #find all matches of this simple regular expression
-  end
+	# lookup frequency of a word, a simple lookup in the @dictionary Hash
+	def lookup word
+		dictionary[word]
+	end
+	
+	def edits1 word
+		r = []
+		
+		# Deletes
+		(0...word.length).each do |i|
+			w = word.dup
+			w[i, 1] = "".freeze
+			r << w
+		end
+		
+		# transposes
+		(1...word.length).each do |i|
+			w = word.dup
+			w[i-1] = word[i]
+			w[i]   = word[i-1]
+			r << w
+		end
+		
+		# inserts
+		ALPHABET.each_char.each do |c|
+			(0..word.length).each do |i|
+				r << word.dup.insert(i, c)
+			end
+		end
+		
+		# replaces
+		ALPHABET.each_char do |c|
+			(0...word.length).each do |i|
+				w = word.dup
+				w[i] = c
+				r << w
+			end
+		end
+		
+		r.uniq!
+		r
+	end
+	
+	def known_edits1 word
+		r = edits1 word
+		known! r
+	end
+	
 
-  #train model (create dictionary)
-  def train!(word_list)
-    #create @dictionary, an attribute of type Hash mapping words to their count in the text {word => count}. Default count should be 0 (argument of Hash constructor).
-  end
+	# find known (in dictionary) distance-2 edits of target word.
+	def known_edits2 word
+		r = edits1(word).map{|edit| known_edits1 edit}
+		r.flatten!
+		r.uniq!
+		r
+	end
 
-  #lookup frequency of a word, a simple lookup in the @dictionary Hash
-  def lookup(word)
-  end
-  
-  #generate all correction candidates at an edit distance of 1 from the input word.
-  def edits1(word)
-    
-    deletes    = []
-    #all strings obtained by deleting a letter (each letter)
-    transposes = []
-    #all strings obtained by switching two consecutive letters
-    inserts = []
-    # all strings obtained by inserting letters (all possible letters in all possible positions)
-    replaces = []
-    #all strings obtained by replacing letters (all possible letters in all possible positions)
-
-    return (deletes + transposes + replaces + inserts).to_set.to_a #eliminate duplicates, then convert back to array
-  end
-  
-
-  # find known (in dictionary) distance-2 edits of target word.
-  def known_edits2 (word)
-    # get every possible distance - 2 edit of the input word. Return those that are in the dictionary.
-  end
-
-  #return subset of the input words (argument is an array) that are known by this dictionary
-  def known(words)
-    return words.find_all {true } #find all words for which condition is true,
-                                    #you need to figure out this condition
-    
-  end
-
-
-  # if word is known, then
-  # returns [word], 
-  # else if there are valid distance-1 replacements, 
-  # returns distance-1 replacements sorted by descending frequency in the model
-  # else if there are valid distance-2 replacements,
-  # returns distance-2 replacements sorted by descending frequency in the model
-  # else returns nil
-  def correct(word)
-  end
-    
-  
+	#return subset of the input words (argument is an array) that are known by this dictionary
+	def known words
+		words.select{|w| dictionary.include? w}
+	end
+	
+	# if word is known, then
+	# returns [word], 
+	# else if there are valid distance-1 replacements, 
+	# returns distance-1 replacements sorted by descending frequency in the model
+	# else if there are valid distance-2 replacements,
+	# returns distance-2 replacements sorted by descending frequency in the model
+	# else returns nil
+	def correct word
+		if dictionary.include? word
+			return [word]
+		end
+		
+		edits = known_edits1 word
+		unless edits.empty?
+			return order! edits
+		end
+		
+		edits = known_edits2 word
+		unless edits.empty?
+			return order! edits
+		end
+	end
+	
+	private
+	
+	def known! words
+		words.select!{|w| dictionary.include? w}
+		words
+	end
+	
+	def order! words
+		words.sort_by! &dictionary
+		words.reverse!
+		words
+	end
 end
 
--- test/models/spellchecker_test.rb
+++ test/models/spellchecker_test.rb
@@ -1,85 +1,59 @@
 require 'test_helper'
 
 class SpellcheckerTest < ActiveSupport::TestCase
+	def setup
+		@sp = load_spellchecker
+	end
+	
+	test "word counting" do
+		assert_equal 3, @sp.lookup("hello")
+		assert_equal 3, @sp.lookup("ruby")
+		assert_equal 2, @sp.lookup("rudy")
+		assert_equal 1, @sp.lookup("rails")
+		assert_equal 0, @sp.lookup("nothing"), "lookup missing words should return 0"
+	end
 
-  
-  test "word counting" do
-    sp = load_spellchecker #helper method to load sample Spellchecker
-    check = sp.lookup("hello")==3 &&
-        sp.lookup("ruby")==3 &&
-        sp.lookup("rudy")==2 &&
-        sp.lookup("rails")==1
-    assert check, "problem with counting words"
-    check = (sp.lookup("nothing")==0)
-    assert check , "lookup missing words should return 0"
-  end
-
-  
-  test "known words" do
-    sp = load_spellchecker #helper method to load sample Spellchecker
-    knowns1 = sp.known(["notthere", "hello", "ruby", "nonono", "ru"])
-    knowns2 = sp.known(["hello", "goodbye", "rude", "yayaya", "ru", "rails"])
-    expected = ["hello", "ruby"]
-    assert knowns1==expected, "known words! got #{knowns1}, expected #{expected}"
-    expected = ["hello", "rude", "rails"]
-    assert knowns2==expected, "known words!  got #{knowns2}, expected #{expected}"
-  end
-  
+	
+	test "known words" do
+		knowns = @sp.known(["notthere", "hello", "ruby", "nonono", "ru"])
+		assert_equal ["hello", "ruby"], knowns
+		
+		knowns = @sp.known(["hello", "goodbye", "rude", "yayaya", "ru", "rails"])
+		assert_equal ["hello", "rude", "rails"], knowns
+	end
+	
 # all the types of 1-distance edits
-  
-  test "deletes" do
-    sp = load_spellchecker #helper method to load sample Spellchecker
-    eds = sp.edits1("hel")
-    #deletes
-    deletes = ["he", "hl", "el"]
-    deletes.each {|d| assert eds.include?(d) , "problem with deletes: test case #{d}" }    
-  end
-  
-  test "inserts" do
-    sp = load_spellchecker #helper method to load sample Spellchecker
-    eds = sp.edits1("hel")
-    #subset of possible includes
-    inserts = ["ahel", "hkel", "herl", "helz"]
-    inserts.each {|d| assert eds.include?(d) , "problem with includes: test case #{d}" }    
-  end
-   
-  test "transposes" do
-    sp = load_spellchecker #helper method to load sample Spellchecker
-    eds = sp.edits1("hel")
-    #tranposes
-    transposes = ["ehl", "hle"]
-    transposes.each {|d| assert eds.include?(d) , "problem with transposes: test case #{d}"}    
-  end
-  
-  test "replaces" do
-    sp = load_spellchecker #helper method to load sample Spellchecker
-    eds = sp.edits1("hel")
-    #subset of replaces
-    replaces = ["vel","hfl", "hep"]
-    replaces.each {|d| assert eds.include?(d), "problem with replaces: test case= #{d}"}    
-  end
-  
-# 
-  test "correct" do
-    sp = load_spellchecker #helper method to load sample Spellchecker
-    
-    eds = sp.correct("rude") #d1 edits exist
-    know =["rude"] #known word
-    
-    assert eds==know, "correction error: known word! Got #{eds}, expected: #{know}"
-    
-    eds = sp.correct("uby") #d1 edits exist
-    e1s =["ruby", #insert
-          "by"] #replace                
-    assert eds == e1s, "correction error: known distance-1 edits! Got #{eds}, expected: #{e1s}"
-    
-    eds = sp.correct("duy") #no d1 edits
-
-    e2s =["ruby", #insert, replace
-          "rudy", #transpose, insert
-          "by"] #delete, delete                
-    assert eds == e2s, "correction error: distance-2 edits! Got #{eds}, expected: #{e2s}"
-
-  end
-
+	
+	test "deletes" do
+		edits = @sp.edits1("hel")
+		assert_includes edits, "he"
+		assert_includes edits, "hl"
+		assert_includes edits, "el"
+	end
+	
+	test "inserts" do
+		edits = @sp.edits1("hel")
+		#subset of possible includes
+		inserts = ["ahel", "hkel", "herl", "helz"]
+		inserts.each {|d| assert edits.include?(d) , "problem with includes: test case #{d}" }		
+	end
+	 
+	test "transposes" do
+		edits = @sp.edits1("hel")
+		assert_includes edits, "ehl"
+		assert_includes edits, "hle"
+	end
+	
+	test "replaces" do
+		edits = @sp.edits1("hel")
+		assert_includes edits, "vel"
+		assert_includes edits, "hfl"
+		assert_includes edits, "hep"
+	end
+	
+	test "correct" do
+		assert_equal ["rude"], @sp.correct("rude")
+		assert_equal ["ruby", "by"], @sp.correct("uby")
+		assert_equal ["ruby", "rudy", "by"], @sp.correct("duy")
+	end
 end
